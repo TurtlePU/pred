5b69072f63d9146a117c88e163b404a4efb1a8f0
TurtlePU <sokolov.p64@gmail.com>
2025-08-13 03:03:05 +0300

    encapsulate freeing of resources

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,7 @@ import Data.List ((!?))
 import Data.Maybe (catMaybes)
 import Data.Ord (clamp)
 import Data.Traversable (for)
+import Foreign.C (CInt)
 
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Resource qualified as Resource
@@ -32,6 +33,32 @@ data Config = MkConfig
   }
   deriving Generic
 
+data FontData m = MkFontData
+  { font :: TTF.Font
+  , fontSurfaces :: [(CInt, SDL.Surface)]
+  , recreate :: (Config -> Config) -> m (FontData m)
+  , saveConfig :: FilePath -> m ()
+  }
+  deriving Generic
+
+newFontData ::
+  Resource.MonadResource m => Config -> [Text] -> m (FontData m)
+newFontData config textLines = do
+  (fontRK, font) <- TTF.load (Text.unpack config.fontPath) config.fontSize
+                      `Resource.allocate` TTF.free
+  (surfacesRK, fontSurfaces) <- unzip . catMaybes <$> for (zip [0..] textLines)
+    \(lineNum, line) ->
+      if Text.null line
+      then pure Nothing
+      else Just . fmap (lineNum ,) <$>
+        (TTF.solid font (SDL.V4 255 255 255 255) line
+          `Resource.allocate` SDL.freeSurface)
+  let freeFont = Resource.release fontRK >> for_ surfacesRK Resource.release
+      recreate update = freeFont >> newFontData (update config) textLines
+      saveConfig configPath =
+        freeFont >> void (Toml.encodeToFile Toml.genericCodec configPath config)
+  pure MkFontData {..}
+
 main :: IO ()
 main = Resource.runResourceT do
   filePath <- liftIO $ Opt.execParser $ Opt.info
@@ -44,47 +71,39 @@ main = Resource.runResourceT do
     True -> Text.lines <$> Text.readFile filePath
     False -> pure []
   SDL.initializeAll
-  configPath <- liftIO $ Dir.getXdgDirectory Dir.XdgConfig "predconfig.toml"
-  initialConfig <- liftIO $ Dir.doesFileExist configPath >>= \case
-    True -> Toml.decodeFile Toml.genericCodec configPath
-    False -> do
-      fc <- FC.initLoadConfigAndFonts
-      pattern <- maybe (error "lol no Fira Code") pure $
-        FC.fontMatch fc (FC.nameParse "Fira Code")
-      fontPath <- maybe (error "lol no filepath") (pure . Text.pack) $
-        FC.getValue "file" pattern
-      pure MkConfig { fontSize = 36, .. }
   _ <- TTF.initialize `Resource.allocate_` TTF.quit
   (_, window) <- SDL.createWindow "PrEd proof editor" SDL.defaultWindow
     { windowHighDPI = True
     , windowMode = SDL.Maximized
     , windowResizable = True
     } `Resource.allocate` SDL.destroyWindow
-  flip fix initialConfig \changeConfig config -> do
-    (fontRK, font) <- TTF.load (Text.unpack config.fontPath) config.fontSize
-                        `Resource.allocate` TTF.free
-    (surfacesRK, fontSurfaces) <- unzip . catMaybes <$>
-      for (zip [0..] textLines) \(lineNum, line) ->
-        if Text.null line
-        then pure Nothing
-        else Just . fmap (lineNum ,) <$>
-          (TTF.solid font (SDL.V4 255 255 255 255) line
-            `Resource.allocate` SDL.freeSurface)
-    let freeFont = Resource.release fontRK >> for_ surfacesRK Resource.release
+  configPath <- liftIO $ Dir.getXdgDirectory Dir.XdgConfig "predconfig.toml"
+  initialFontData <- do
+    config <- liftIO $ Dir.doesFileExist configPath >>= \case
+      True -> Toml.decodeFile Toml.genericCodec configPath
+      False -> do
+        fc <- FC.initLoadConfigAndFonts
+        pattern <- maybe (error "lol no Fira Code") pure $
+          FC.fontMatch fc (FC.nameParse "Fira Code")
+        fontPath <- maybe (error "lol no filepath") (pure . Text.pack) $
+          FC.getValue "file" pattern
+        pure MkConfig { fontSize = 36, .. }
+    newFontData config textLines
+  flip fix initialFontData \changeData fontData -> do
     flip fix (SDL.V2 0 0) \loop (SDL.V2 colPos linePos) -> do
       windowSurface <- SDL.getWindowSurface window
       SDL.surfaceFillRect windowSurface Nothing (SDL.V4 0 0 0 255)
-      lineSkip <- toEnum <$> TTF.lineSkip font
+      lineSkip <- toEnum <$> TTF.lineSkip fontData.font
       colSkip <- case textLines !? fromIntegral linePos of
         Nothing -> pure 0
         Just line -> do
           let pos = fromIntegral colPos
               start = Text.take pos line
-          (trueWidth, _) <- TTF.size font (Text.take pos line)
-          Just (_, _, _, _, advance) <- TTF.glyphMetrics font 'o'
+          (trueWidth, _) <- TTF.size fontData.font (Text.take pos line)
+          Just (_, _, _, _, advance) <- TTF.glyphMetrics fontData.font 'o'
           pure $ trueWidth + advance * max 0 (pos - Text.length start)
       SDL.V2 _ windowHeight <- SDL.surfaceDimensions windowSurface
-      for_ fontSurfaces \(i, fontSurface) -> do
+      for_ fontData.fontSurfaces \(i, fontSurface) -> do
         let blitY = (i - fromIntegral linePos) * lineSkip
             blitPos = SDL.V2 (-toEnum colSkip) blitY
         if 0 <= blitY && blitY < windowHeight
@@ -94,12 +113,15 @@ main = Resource.runResourceT do
       SDL.updateWindowSurface window
       event <- SDL.waitEvent
       case interestingEvent event of
-        KeyPress SDL.KeycodeQ      ->
-          void (Toml.encodeToFile Toml.genericCodec configPath config)
-        KeyPress SDL.KeycodeEquals -> freeFont >> changeConfig config
-          { fontSize = config.fontSize + 1 }
-        KeyPress SDL.KeycodeMinus  -> freeFont >> changeConfig config
-          { fontSize = config.fontSize - 1 }
+        KeyPress SDL.KeycodeQ      -> fontData.saveConfig configPath
+        KeyPress SDL.KeycodeEquals -> do
+          fontData' <- fontData.recreate \config -> config
+            { fontSize = config.fontSize + 1 }
+          changeData fontData'
+        KeyPress SDL.KeycodeMinus  -> do
+          fontData' <- fontData.recreate \config -> config
+            { fontSize = config.fontSize - 1 }
+          changeData fontData'
         MouseScroll (SDL.V2 dx dy) -> loop $ SDL.V2
           (clamp (0, fromIntegral $ maximum $ map Text.length textLines)
                  (colPos + dx))
